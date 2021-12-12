42c9a412e0637a070aa0100eb4eaa819802be5f7
Carlos Mancini <carlos@devsquad.email>
2021-12-12 18:04:49 -0300

    Adicionando font-awesome ao projeto

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 
-createApp(App).use(router).mount('#app')
+library.add(faUserSecret)
+createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
